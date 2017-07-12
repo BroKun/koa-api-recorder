64fbfef455bbcd454bcdc6f248c4a52515f3cad4
王玉坤 <wang_yk@hzzh.com>
2017-07-12 20:14:43 +0800

    testPath处理

--- test/test.js
+++ test/test.js
@@ -9,11 +9,11 @@ const fs = require('fs');
 const bodyParser = require('koa-bodyparser');
 const getHttp = require('http').Server;
 
-
+const testPath = path.join(__dirname, '../');
 const app = new koa();
 const server = getHttp(app.callback());
 app.use(bodyParser());
-app.use(recorder({ dirPath: path.join(__dirname, '../'), postMan: true }));
+app.use(recorder({ dirPath: testPath, postMan: true }));
 app.use(async (ctx, next) => {
   ctx.body = { test: 'test body' };
   await next();
@@ -30,13 +30,13 @@ describe('koa-api-recorder', () => {
       .end((err, res) => {
         if (err) return done(err);
         assert(res.body.test === 'test body');
-        assert(fs.existsSync(path.join(__dirname, '../files/koa-api-recorder/history.json')) === true);
-        assert(fs.existsSync(path.join(__dirname, '../files/koa-api-recorder/postman.json')) === true);
-        const historyStr = fs.readFileSync(path.join(__dirname, '../files/koa-api-recorder/history.json'), 'utf-8');
+        assert(fs.existsSync(path.join(testPath, 'koa-api-recorder/history.json')) === true);
+        assert(fs.existsSync(path.join(testPath, 'koa-api-recorder/postman.json')) === true);
+        const historyStr = fs.readFileSync(path.join(testPath, 'koa-api-recorder/history.json'), 'utf-8');
         assert(historyStr.indexOf('test body') > -1);
         assert(historyStr.indexOf('json') > -1);
         assert(historyStr.indexOf('bar') > -1);
-        const postmanStr = fs.readFileSync(path.join(__dirname, '../files/koa-api-recorder/history.json'), 'utf-8');
+        const postmanStr = fs.readFileSync(path.join(testPath, 'koa-api-recorder/history.json'), 'utf-8');
         assert(postmanStr.indexOf('test body') > -1);
         assert(postmanStr.indexOf('json') > -1);
         assert(postmanStr.indexOf('bar') > -1);
